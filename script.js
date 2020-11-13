20b97be4ba48c120022502e85c9634bc4980d4d9
Chris Cooley <chriscooley1984@yahoo.com>
2020-11-12 22:17:54 -0700

    added variables to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var hour9 = $("#9AM");
+var hour10 = $("#10AM");
+var hour11 = $("#11AM");
+var hour12 = $("#12PM");
+var hour1 = $("#1PM");
+var hour2 = $("#2PM");
+var hour3 = $("#3PM");
+var hour4 = $("#4PM");
+var hour5 = $("#5PM");
+var now = moment();
+
